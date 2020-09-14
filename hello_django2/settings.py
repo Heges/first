eafa532038cdf53a4518509e3566979702f74918
Shy <ikunkka92@gmail.com>
2020-09-14 17:52:35 +0300

    ctrl+alt+l pep8

--- hello_django2/settings.py
+++ hello_django2/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = 'l%8_!^ze-*amhpb!xi^&_%oml2ze4(zljd&srd+p4y7eywygzg'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -70,21 +68,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hello_django2.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-            "default": {
-                    "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-                    "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-                    "USER": os.environ.get("SQL_USER", "user"),
-                    "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-                    "HOST": os.environ.get("SQL_HOST", "localhost"),
-                    "PORT": os.environ.get("SQL_PORT", "5432"),
-            }
-        }
-
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -104,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -117,7 +112,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
